31d2d4120eef03a9bea6031301214e41f8f26428
Tvildiani1 <tvildianitvildiani@yahoo.com>
2025-07-22 09:54:10 +0400

    feat/musicCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import MusicCard from "./components/musicCard/MusicCard";
 import PopularCard from "./components/popularSongCard/SongCard";
 
 
@@ -5,7 +6,7 @@ import PopularCard from "./components/popularSongCard/SongCard";
 export default function Home() {
   return (
     <div>
-        
+      
     </div>
   );
 }
