c6a156b1ddefdf3fa05767a47cbb5afcff95bb12
Tvildiani1 <tvildianitvildiani@yahoo.com>
2025-07-30 09:36:20 +0400

    feat/playerUpdate

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Player from './components/player/Player'
+
 
 export default function Home() {
   return (
     <div>
-      <Player icon='' artistName='' songName=''/>
+      
     </div>
   );
 }
